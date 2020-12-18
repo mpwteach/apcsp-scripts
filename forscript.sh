9adb4e37068d2440d3b10ea20cd7c21c6e524389
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2020-12-18 21:39:38 +0000

    new files

---
+++ forscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "hello"
+
+for file in * ; do
+    echo $file
+    echo "would you like to view $file? (y/n):"
+    read ANS
+    if [ $ANS = "y" ] ; then
+        cat $file
+    fi
+done
+
+
+
+exit 0
